7a6fda8963115066c80172f85f54d32a99be7c27
Balazs Makrai <b.makrai@gmail.com>
2022-12-06 17:56:24 +0100

    add layout

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore praesentium tempora aperiam labore quibusdam
+            porro, ad accusamus modi accusantium debitis.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    font-size: 36px;
+    margin-bottom: 20px
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Product detail for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia enim harum labore vel aliquam. Qui.</p>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'products'
+});
+const { id } = useRoute().params;
+</script>
+
+<style scoped>
+
+</style>
